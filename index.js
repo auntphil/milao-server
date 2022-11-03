e000d9699e5d7e0a9d9cbed2a3da301562253b4f
Andrew <andhochmuth@gmail.com>
2022-11-03 07:14:42 -0400

    Added dotenv and jsonwebtoken

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ import { startStandaloneServer } from '@apollo/server/standalone';
 import { mongoose } from 'mongoose'
 import { typeDefs } from './schema.js'
 import { resolvers } from './resolvers.js'
+import * as dotenv from 'dotenv'
+
+dotenv.config()
 
 mongoose.connect(`mongodb://192.168.0.51:7017/milao`)
     .then( () => {
